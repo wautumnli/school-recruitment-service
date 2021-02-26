33245f4a8051d0eaa50b4e01004fa5ae5b1b2a9a
万秋里 <wanqiuli@jd.com>
2021-02-26 10:28:04 +0800

    主启动类添加

---
+++ recruitment-web/src/main/java/com/ql/recruitment/RecruitmentWebApplication.java
@@ -0,0 +1,16 @@
+package com.ql.recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author： wanqiuli
+ * DateTime: 2021/2/26 9:33
+ */
+@SpringBootApplication
+public class RecruitmentWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentWebApplication.class, args);
+    }
+}
